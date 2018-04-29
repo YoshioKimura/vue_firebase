9ddee45307c587f83fd4ad6b87ea28dc8f543bb0
gsgitsample <thk.rspt.csd@gamil.com>
2018-04-29 20:52:09 +0900

    initial commit

---
+++ mymarkdown/src/components/Editor.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="editor">
+    <h1>エディター画面</h1>
+    <span>{{  }}</span>
+    <button @click="logout"> ログアウト </button>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'editor',
+    props: ['user'],
+    data() {
+      return {}
+    },
+    methods: {
+      logout: function () {
+        firebase.auth().signOut();
+      }
+    }
+  }
+
+</script>
---
+++ mymarkdown/src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="home">
+    <h1>{{ msg }}</h1>
+    <button @click="googleLogin">Google アカウントでログイン </button>   
+  </div>
+ </template>
+ <script>  export default {   
+   name: 'home',   
+   data () {
+      return {
+   }
+  },
+  methods: {
+  googleLogin: function() {
+     firebase.auth().signInWithRedirect(new firebase.auth.GoogleAuthProvider());
+  }
+ }
+ }  
+ </script>
